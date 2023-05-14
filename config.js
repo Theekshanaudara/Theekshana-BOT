03bef00d074ce440dd8ca6c72478cd1400a71ee4
Theekshana udara <88057292+Theekshanaudara@users.noreply.github.com>
2023-05-14 17:36:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741597760', 'Owner', true],
+  ['94741597760', 'Theekshana', true], 
   [''] 
 ] //Number of owners
 
@@ -47,10 +47,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'Gaming Theeksh-yt'
+global.packname = 'Tikka┃ᴮᴼᵀ' 
+global.author = '@Theekshana' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
